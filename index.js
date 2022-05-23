3a85f9ce0ccb12d20395d69118366bf3319bbc99
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-23 12:37:07 +0300

    feat: add eslit

---
+++ index.js
@@ -0,0 +1,16 @@
+const name1 = "Serg"
+const name = 'James';
+console.log("%c 👩‍🎓: name ", "font-size:16px;background-color:#b96275;color:white;", name);
+const data = {
+  name: "Kat",
+  city: "Rivc",
+  city: "Rivc"
+}
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
